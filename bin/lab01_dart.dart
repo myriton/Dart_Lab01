c2d5b31da6f7be7146229d469ade27f2d9c80b61
myriton <nikasevcenko642@gmail.com>
2026-09-13 20:30:40 +0300

    Initial dart project

---
+++ bin/lab01_dart.dart
@@ -0,0 +1,5 @@
+import 'package:lab01_dart/lab01_dart.dart' as lab01_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab01_dart.calculate()}!');
+}
---
+++ test/lab01_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab01_dart/lab01_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
